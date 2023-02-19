e11da623f8090ba6d058904c416f252f0a437cf4
Elaine-G17 <yilingu2023@u.northwestern.edu>
2023-02-19 03:48:02 +0000

    1st

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,24 @@
+class PlacesController < ApplicationController
+
+def index
+    @place = Place.all
+    @post = Post.all
+end
+    
+def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @post= Post.where({"place_id" => @place["id"]})
+end
+    
+def new
+    @place= Place.new
+end
+      
+def create
+    @place = Place.new
+    @place["name"] = params["place"]["name"]
+    @place.save
+redirect_to "/places"
+end
+
+end
